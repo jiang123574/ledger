30d3c95ad2311a2b6967fc6eeb298faa29b41750
ClegeA <jiang123574@163.com>
2026-04-19 11:09:20 +0800

    cleanup: remove orphaned page-skeleton and page-transition controllers
    - Delete page_skeleton_controller.js and page_transition_controller.js
    - Remove imports and registrations from index.js and stimulus-loading.js
    - Remove pins from importmap.rb
    - Remove page-fade, page-skeleton, skeleton-block CSS from application.css and tailwind.css
    - Related: TODO.md #29

--- app/javascript/stimulus-loading.js
+++ app/javascript/stimulus-loading.js
@@ -13,8 +13,6 @@ import ListFilterController from "controllers/list_filter_controller"
 import LoadingButtonController from "controllers/loading_button_controller"
 import MenuController from "controllers/menu_controller"
 import MobileLayoutController from "controllers/mobile_layout_controller"
-import PageSkeletonController from "controllers/page_skeleton_controller"
-import PageTransitionController from "controllers/page_transition_controller"
 import SankeyChartController from "controllers/sankey_chart_controller"
 import SelectController from "controllers/select_controller"
 import SparklineChartController from "controllers/sparkline_chart_controller"
@@ -34,8 +32,6 @@ application.register("list-filter", ListFilterController)
 application.register("loading-button", LoadingButtonController)
 application.register("menu", MenuController)
 application.register("mobile-layout", MobileLayoutController)
-application.register("page-skeleton", PageSkeletonController)
-application.register("page-transition", PageTransitionController)
 application.register("sankey-chart", SankeyChartController)
 application.register("select", SelectController)
 application.register("sparkline-chart", SparklineChartController)
