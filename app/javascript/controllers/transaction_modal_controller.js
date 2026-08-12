95b931410323b6f95730cb50575eb48ae9a9f837
ClegeA <jiang123574@163.com>
2026-08-12 16:33:03 +0800

    fix: focus amount input after save and continue

--- app/javascript/controllers/transaction_modal_controller.js
+++ app/javascript/controllers/transaction_modal_controller.js
@@ -440,7 +440,11 @@ export default class extends Controller {
           showSuccessToast(data.message || '交易已创建，请继续录入')
           const amountInput = form.querySelector('input[name="transaction[amount]"]')
           const noteInput = form.querySelector('input[name="transaction[note]"]')
-          if (amountInput) amountInput.value = ''
+          if (amountInput) {
+            amountInput.value = ''
+            amountInput.focus()
+            amountInput.select()
+          }
           if (noteInput) noteInput.value = ''
 
           const urlParams = new URLSearchParams(window.location.search)
